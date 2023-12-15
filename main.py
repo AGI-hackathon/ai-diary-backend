0fc7a2cf26b0e07c749ea3a4993df5167b16ac78
天才 <18320122+incomingflyingbrick@users.noreply.github.com>
2023-12-15 11:17:58 +0800

    added logic to api

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from typing import Union
 from pydantic import BaseModel
-
 from fastapi import FastAPI
+import pymongo
+import random
+import json
 
+# mongoDB connection string:
+mongo_conn_str = 'mongodb+srv://agi-user-prod:O0lfjQSLEi6v423z@agi-diary-cluster.mtxn3qw.mongodb.net/'
+client = pymongo.MongoClient(mongo_conn_str)
+py_db = client['agi-db']
 # This app is based on python version 3.9.6
 # run local
 # pip install -r requirements.txt
@@ -22,19 +28,25 @@ class Diary(BaseModel):
 
 @app.post("/diary/upload", description="上传日记")
 def upload_diary(diary: Diary):
+    py_db['diary'].insert_one(diary.model_dump())
     return {"upload_status": "success"}
 
 
 @app.get("/diary/fetch", description="拉取日记")
 def fetch_diary():
     diary_list = []
+    all_diary = py_db['diary'].find()
+    for diary in all_diary:
+        diary['_id'] = str(diary['_id'])
+        diary_list.append(diary)
     return {"diary_list": diary_list}
 
 
 @app.get("/diary/emotion", description="获取现在心情")
 def get_emotion(current_text: str):
-    # TODO fetch emotion from GPT
-    return {"mood": "happy"}
+    moods = ["happy", "sad", "cry", "high", "low"]
+    mood = random.choice(moods)
+    return {"mood": mood}
 
 
 @app.get("/", description="心跳测试接口")
